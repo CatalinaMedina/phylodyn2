b54defec471d9faaae0d85980881cf73e4b66bde
CatalinaMedina <catalmm1@uci.edu>
2022-02-15 11:34:54 -0800

    added joint_stats

--- R/infer_coal.R
+++ R/infer_coal.R
@@ -1,3 +1,47 @@
+#' joint_stats Title TODO
+#'
+#' @param coal_data data frame with columns: time, event, E_log
+#' @param samp_data data frame with columns: time, count, E_log
+#'
+#' @return list with \describe{
+#'   \item{Y}{matrix with two columns. The first column contains the coalescence events padded with NA's at end. The second column contains the sampling counts, padded by NA's at the beginning.}
+#'   \item{beta0}{numeric vector with 0's for length of coal_data$time and 1's for length of samp_data$time; for sampling intensity function}
+#'   \item{time}{coalescence times padded with 0's at end}
+#'   \item{time2}{sampling times padded with 0's at beginning}
+#'   \item{w}{numeric vector; 1's repeated for length of coal_data$time followed by -1's repeated for length of samp_data$time}
+#'   \item{E_log}{coalescence E_log followed by sampling E_log}
+#' }
+#'
+joint_stats <- function(coal_data, samp_data){
+  n1 <- length(coal_data$time)
+  n2 <- length(samp_data$time)
+  
+  beta0 <- c(rep(0, n1), rep(1, n2))
+  
+  E_log <- c(coal_data$E_log, samp_data$E_log)
+  
+  Y <- matrix(
+    c(coal_data$event, rep(NA, n2), rep(NA, n1), samp_data$count),
+    nrow = n1 + n2, 
+    byrow = FALSE
+  )
+  
+  w <- c(rep(1, n1), rep(-1, n2))
+  
+  time  <- c(coal_data$time, rep(NA, n2))
+  time2 <- c(rep(NA, n1), samp_data$time)
+  
+  list(
+    Y = Y, 
+    beta0 = beta0, 
+    time = time, 
+    time2 = time2, 
+    w = w, 
+    E_log = E_log
+  )
+}
+
+
 #' samp_stats Title TODO
 #'
 #' @param grid numeric vector; TODO
